5284151fb8812a376d95cfacd002c58523160702
potlurimounika <mounikapotluri153@gmail.com>
2025-02-14 09:26:43 +0530

    Create 04.js

---
+++ 04.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class MyApp extends Component
+{
+ render()
+ {
+  return (
+    <div>
+      <p class Name="MyCalci">{2+2}</p>
+    </div>
+  );
+ }
+}
+export default MyApp;
