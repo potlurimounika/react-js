55ec0afa982a6ed24683e514458fb1d74e421cae
potlurimounika <mounikapotluri153@gmail.com>
2025-02-14 09:20:06 +0530

    Create 03.js

---
+++ 03.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class App extends Component
+{
+ render()
+ {
+  return (
+    <div>
+      <h1>Nipuna technologies.</h1>
+      <h2>Training center</h2>
+      <p>No.1 Training center</p>
+    </div>
+  );
+ }
+}
+export default App;
